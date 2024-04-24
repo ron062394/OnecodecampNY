1c48028103bb6b540aefd9c10d6c72a48f11290b
ron062394 <ron062394@gmail.com>
2024-04-24 08:54:57 +0800

    6 - JavaScript

---
+++ 6 - JavaScript/assignment-expressions.js
@@ -0,0 +1,16 @@
+//4. Assignment expressions
+let a = 10;
+let b = 5;
+
+a += b;
+console.log("The new value of a is:", a);
+
+
+a -= b;
+console.log("The new value of a is:", a);
+
+a *= b;
+console.log("The new value of a is:", a);
+
+a /= b;
+console.log("The new value of a is:", a);
---
+++ 6 - JavaScript/logical-expressions.js.js
@@ -0,0 +1,16 @@
+// String Expressions or concatenation
+console.log("Hello" + " " + "World!");
+let greeting = "Hi";
+let myName = "John";
+console.log(greeting + " " + myName + ".");
+
+
+
+// Logical expressions (&&, ||, !)
+let sunny = true;
+let warm = true;
+console.log("Is it sunny AND warm?", sunny && warm);
+console.log("Is it sunny OR warm?", sunny || warm);
+console.log("NOT sunny?", !sunny);
+
+
